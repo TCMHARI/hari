6d4005fcf358d013d1c5bc28be6abc89bae5c4ea
TCMHARI <20047622@mydbs.ie>
2024-11-21 19:34:16 +0000

    db conne

---
+++ main.js
@@ -0,0 +1,22 @@
+let editMemberId = null;
+let db;
+
+document.addEventListener("DOMContentLoaded", function () {
+    let request = indexedDB.open("memberDatabase", 1);
+
+    request.onupgradeneeded = function (event) {
+        db = event.target.result;
+        if (!db.objectStoreNames.contains("members")) {
+            db.createObjectStore("members", { keyPath: "id", autoIncrement: true });
+        }
+    };
+
+    request.onsuccess = function (event) {
+        db = event.target.result;
+        console.log("Database opened successfully");
+        renderMembers();
+    };
+
+    request.onerror = function (event) {
+        console.error("Database error: ", event.target.errorCode);
+    };
