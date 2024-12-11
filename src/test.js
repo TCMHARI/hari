c6a3b0488681241524f5212915a0b0c90babfdb3
TCMHARI <20047622@mydbs.ie>
2024-12-11 18:53:52 +0000

    post a new member

--- src/test.js
+++ src/test.js
@@ -19,3 +19,23 @@ describe('Members API', () => {
                 done();
             });
     });
+    it('should POST a new member', (done) => {
+        let member = {
+            name: "John Wick",
+            age: 30,
+            gender: "Male",
+            phone: "0187654321",
+            email: "john@user.com",
+            membershipType: "Annual"
+        };
+        chai.request(app)
+            .post('/api/members')
+            .send(member)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('name').eql('John Wick');
+                memberId = res.body.id;
+                done();
+            });
+    });
